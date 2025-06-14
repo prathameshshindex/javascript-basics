9f2ad95009b06cf463f3bf077c55857547e2e330
prathameshshindex <prathmeshshinde991@gmail.com>
2025-06-14 05:32:50 +0000

    datatypes

---
+++ 01_basics/test.js
@@ -0,0 +1,17 @@
+console.log("Hello, World!");
+
+const account = 1438985;
+let accountEmail = "diog@gmail.com";
+var accountpassword = '123345';
+let accountstate;
+
+//account = 2;
+accountEmail = 'gffd@gmail.com';
+accountpassword = '56656';
+//console.log(account,accountEmail, accountpassword);
+console.table([account, accountEmail, accountpassword]);
+/*
+prefer const for constants, let for variables that will change, and var for function-scoped variables.
+*/
+
+
---
+++ 02_basics/datatypes.js
@@ -0,0 +1,6 @@
+//alert(3+3) we are using nodejs, not broweser
+let name = "prathamesh";
+let age = 20;
+let isLoggedIn = true;
+//bigint
+console.log(typeof name); // string
