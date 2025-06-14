f42216a58ffb8b90cb6a48b17e7c883ca788e748
prathameshshindex <prathmeshshinde991@gmail.com>
2025-06-14 05:49:15 +0000

    conversion

---
+++ 02_basics/03_coversionOperation.js
@@ -0,0 +1,8 @@
+let score = "33";
+console.log(typeof score); 
+console.log(typeof(score)); 
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); 
+
