da401d4b0e58be0bced676e9cf0e03da24415e6d
prathameshshindex <prathmeshshinde991@gmail.com>
2025-06-14 06:56:32 +0000

    operators

--- 02_basics/03_coversionOperation.js
+++ 02_basics/03_coversionOperation.js
@@ -1,8 +1,28 @@
 let score = "33";
-console.log(typeof score); 
-console.log(typeof(score)); 
+// console.log(typeof score); 
+// console.log(typeof(score)); 
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber); // number
-console.log(valueInNumber); 
+//console.log(typeof valueInNumber); // number
+//console.log(valueInNumber); 
+
+//******************************operations***************** */
+let value = 3;
+let negvalue = -value;
+console.log(negvalue); // -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2); // 0
+// console.log(2**2); // 4
+
+console.log(1+"2"); // 12
+console.log("1"+2); // 12
+console.log("1"+2+3); // 123
+console.log(1+2+"3"); // 33
+
+
+
 
